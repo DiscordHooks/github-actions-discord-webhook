8567815528d1dac9fca621874f14857eb3e24d9f
Lucas Combs <l-combs@hotmail.com>
2019-09-15 17:13:43 -0400

    Setup base Github actions webhook for testing 99% of this code came from: * https://github.com/DiscordHooks/travis-ci-discord-webhook

---
+++ send.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+if [ -z "$2" ]; then
+  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\nFor details & guide, visit: https://github.com/DiscordHooks/travis-ci-discord-webhook" && exit
+fi
+
+echo -e "[Webhook]: Sending webhook to Discord...\\n";
+
+AVATAR=https://github.githubassets.com/images/modules/logos_page/Octocat.png
+
+case $1 in
+  "success" )
+    EMBED_COLOR=3066993
+    STATUS_MESSAGE="Passed"
+    ;;
+
+  "failure" )
+    EMBED_COLOR=15158332
+    STATUS_MESSAGE="Failed"
+    ;;
+
+  * )
+    STATUS_MESSAGE="Status Unknown"
+g    EMBED_COLOR=0
+    ;;
+esac
+
+AUTHOR_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%aN")"
+COMMITTER_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%cN")"
+COMMIT_SUBJECT="$(git log -1 "$GITHUB_SHA" --pretty="%s")"
+COMMIT_MESSAGE="$(git log -1 "$GITHUB_SHA" --pretty="%b")" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g'
+
+if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
+  CREDITS="$AUTHOR_NAME authored & committed"
+else
+  CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
+fi
+
+#if [[ $TRAVIS_PULL_REQUEST != false ]]; then
+#  URL="https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST"
+#else
+#  URL=""
+#fi
+
+TIMESTAMP=$(date --utc +%FT%TZ)
+WEBHOOK_DATA='{
+  "username": "",
+  "avatar_url": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+  "embeds": [ {
+    "color": '$EMBED_COLOR',
+    "author": {
+      "name": "Job #'"$GITHUB_REF"' (Build #'"$TRAVIS_BUILD_NUMBER"') '"$STATUS_MESSAGE"' - '"$TRAVIS_REPO_SLUG"'",
+      "url": "https://github.com/unthreaded/git-hooks/actions",
+      "icon_url": "'$AVATAR'"
+    },
+    "title": "'"$COMMIT_SUBJECT"'",
+    "url": "'"$URL"'",
+    "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+    "fields": [
+      {
+        "name": "Commit",
+        "value": "'"[\`${GITHUB_SHA:0:7}\`](https://github.com/$TRAVIS_REPO_SLUG/commit/$GITHUB_SHA)"'",
+        "inline": true
+      },
+      {
+        "name": "Branch",
+        "value": "'"[\`$TRAVIS_BRANCH\`](https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_BRANCH)"'",
+        "inline": true
+      }
+    ],
+    "timestamp": "'"$TIMESTAMP"'"
+  } ]
+}'
+
+(curl --fail --progress-bar -A "GitHub-Actions-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "${WEBHOOK_DATA//	/ }" "$2" \
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
